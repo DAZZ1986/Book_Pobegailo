be3a7c9d9ec36b43a50a4be49f82f20900eb6a79
DAZZ1986 new git <dvdzebra@yandex.ru>
2022-03-02 17:04:59 +0300

    update

--- Book_Pobegailo/Book_Pobegailo.cpp
+++ Book_Pobegailo/Book_Pobegailo.cpp
@@ -2,9 +2,8 @@
 #include <iostream>
 using namespace std;
 
-
-
-
+/*
+// 3.2. Создание потока
 volatile int n;
 
 DWORD WINAPI Add(LPVOID iNum)
@@ -15,12 +14,26 @@ DWORD WINAPI Add(LPVOID iNum)
 
     return 0;
 }
+*/
 
 
+
+// 3.3. Завершение потока
+volatile UINT count;
+
+void thread()
+{
+    for(;;)
+    {
+        ++count;
+        Sleep(100); //немного отдохнем
+    }
+}
 
 
 int main()
-{
+{   /*
+    // 3.2. Создание потока
     int inc = 10;
     HANDLE hThread;
     DWORD IDThread;
@@ -28,7 +41,7 @@ int main()
     cout << "n = " << n << endl;
 
     //запускаем поток
-    hThread = CreateThread(NULL, 0, Add, (void*)inc, 0, &IDThread);
+    hThread = CreateThread(NULL, 0, Add, (void*)inc, 0, &IDThread);     //ЖЕНЯ - синтаксис 3 параметра (стр. 44)
     if (hThread == NULL)
         return GetLastError();
 
@@ -39,6 +52,44 @@ int main()
     CloseHandle(hThread);
 
     cout << "n = " << n << endl;
+    */
+
+
+
+
+    // https://www.programmersforum.ru/showthread.php?t=219422
+    // 3.3. Завершение потока
+    HANDLE hThread;
+    DWORD IDThread;
+    char c;
+
+    hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread, NULL, 0, &IDThread);
+
+    if (hThread == NULL)
+        return GetLastError();
+
+    for (;;)
+    {
+        std::cout << "Input 'y' to display the count or any char to finish: ";
+        cin >> c;
+        if (c == 'y')
+            cout << "count = " << count << endl;
+        else
+            break;
+    }
+
+    //прерываем выполнение потока thread 
+    TerminateThread(hThread, 0);
+
+    //закрываем дескриптор потока
+    CloseHandle(hThread);
+
+
+
+
+
+
+
 
 
 
